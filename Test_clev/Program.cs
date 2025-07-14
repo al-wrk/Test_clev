6180a6c143ea967c96cdec32054d39990e4f3c42
al-wrk <al-wrk-accnt@ya.ru>
2025-07-14 03:24:13 +0300

    String Compression Task
    Solution for Task 1: Comress/Decompress string

---
+++ Test_clev/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_clev
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string s = "aabccde";
+            var compressed = s.Compress();
+            var decompressed = compressed.Decompress();
+
+            Console.WriteLine(s);
+            Console.WriteLine(compressed);
+            Console.WriteLine(decompressed);
+        }
+    }
+}
---
+++ Test_clev/StringCompressionExtentions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_clev
+{
+    public static class StringCompressionExtentions
+    {
+        public static string Compress(this string s)
+        {
+            StringBuilder sb = new StringBuilder(s);
+            int i = 0;
+            while (i < sb.Length)
+            {
+                int startPos = i;
+                int grouplenght = 1;
+                while (i < sb.Length - 1 && sb[i] == sb[i+1])
+                {
+                    grouplenght++;
+                    i++;
+                }
+                if (grouplenght > 1)
+                {
+                    sb.Remove(startPos + 1, grouplenght - 1);
+                    sb.Insert(startPos + 1, grouplenght);
+                    i = startPos + grouplenght.ToString().Length;
+                }
+                i++;
+            }
+            return sb.ToString();
+        }
+        public static string Decompress(this string s)
+        {
+            StringBuilder sb = new StringBuilder(s);
+            int i = 0;
+            while(i < sb.Length)
+            {
+                int startPos = i; // первая цифра
+                int groupLenght = 0;
+                while(i < sb.Length && Char.IsDigit(sb[i]))
+                {
+                    groupLenght *= 10;
+                    groupLenght += sb[i] - '0';
+                    i++;
+                }
+                if(groupLenght != 0)
+                {
+                    sb.Remove(startPos, groupLenght.ToString().Length);
+                    char symbol = sb[startPos - 1];
+                    sb.Insert(
+                        index: startPos,
+                        value: Enumerable.Repeat(symbol, groupLenght - 1).ToArray()
+                        );
+                    i = startPos + groupLenght - 1;
+                }
+                i++;
+            }
+            return sb.ToString();
+        }
+    }
+}
