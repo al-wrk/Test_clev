30edcd1b0fd9ae64a2ae34a202a8d76e7e8a8124
al-wrk <al-wrk-accnt@ya.ru>
2025-07-14 03:40:26 +0300

    Server task
    Solution for Task 2: Server

---
+++ Test_clev/Server.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading;
+
+namespace Test_clev
+{
+    public static class Server
+    {
+        private static int count = 0;
+        private static readonly ReaderWriterLockSlim locker = new ReaderWriterLockSlim();
+
+        public static int GetCount()
+        {
+            try
+            {
+                locker.EnterReadLock();
+                return count;
+            }
+            finally
+            {
+                locker.ExitReadLock();
+            }
+        }
+
+        public static void AddToCount(int value)
+        {
+            try
+            {
+                locker.EnterWriteLock();
+                count += value;
+            }
+            finally
+            {
+                locker.ExitWriteLock();
+            }
+        }
+    }
+}
